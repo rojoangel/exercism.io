6af543717879ce4be8671c809fa6fd89b44d34a7
rojoangel <rojo.angel@gmail.com>
2015-08-08 22:03:26 +0200

    hamming exercise

---
+++ php/hamming/hamming.php
@@ -0,0 +1,34 @@
+<?php
+
+function distance($strandA, $strandB)
+{
+    guardSameStrandLength($strandA, $strandB);
+
+    if (empty($strandA) && empty($strandB)) {
+        return 0;
+    }
+
+    return areDifferent(substr($strandA, 0, 1), substr($strandB, 0, 1))
+        + distance(substr($strandA, 1), substr($strandB, 1));
+}
+
+/**
+ * @param $strandA
+ * @param $strandB
+ */
+function guardSameStrandLength($strandA, $strandB)
+{
+    if (strlen($strandA) != strlen($strandB)) {
+        throw new InvalidArgumentException('DNA strands must be of equal length.');
+    }
+}
+
+/**
+ * @param $nucleotideA
+ * @param $nucleotideB
+ * @return int
+ */
+function areDifferent($nucleotideA, $nucleotideB)
+{
+    return (int)!($nucleotideA === $nucleotideB);
+}
