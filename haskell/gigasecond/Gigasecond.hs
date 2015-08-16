db0774e1141323e39c0dc5af811b6e42f5ff19ea
rojoangel <rojo.angel@gmail.com>
2015-08-16 11:21:21 +0200

    haskell - gigasecond - it. 2 - fix a warning in the gigasecond calculation

--- haskell/gigasecond/Gigasecond.hs
+++ haskell/gigasecond/Gigasecond.hs
@@ -3,7 +3,7 @@ module Gigasecond where
 import Data.Time.Clock (NominalDiffTime, UTCTime, addUTCTime)
 
 gigaSecond :: NominalDiffTime
-gigaSecond = (10 ^ 9)
+gigaSecond = 1.0e9
 
 fromDay :: UTCTime -> UTCTime
 fromDay day = addUTCTime gigaSecond day
