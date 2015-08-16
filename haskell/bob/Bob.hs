4cad259d5f480a32abb0ef820167e4af6563587d
rojoangel <rojo.angel@gmail.com>
2015-08-16 23:43:33 +0200

    haskell - bob - it. 1 using guards and where

---
+++ haskell/bob/Bob.hs
@@ -0,0 +1,13 @@
+module Bob (responseFor) where
+
+import Data.Char
+
+responseFor :: String -> String
+responseFor xs
+    | silence   = "Fine. Be that way!"
+    | yell      = "Whoa, chill out!"
+    | question  = "Sure."
+    | otherwise = "Whatever."
+    where silence  = all isSpace xs
+          yell     = (any isAlpha xs) && not (any isLower xs)
+          question = last xs == '?'
