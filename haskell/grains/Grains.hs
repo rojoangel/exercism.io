0e642f78e76ac65130a50dc727f806e336df8fe2
rojoangel <rojo.angel@gmail.com>
2015-08-16 13:08:52 +0200

    haskell - grains - it. 2 using geommetric series sum

--- haskell/grains/Grains.hs
+++ haskell/grains/Grains.hs
@@ -1,8 +1,8 @@
 module Grains (square, total) where
 
 square :: Integer -> Integer
-square 1 = 1
-square i = 2 * square (i - 1)
+square i = 2 ^ (i - 1)
 
+-- geommetric series sum = a*(r^n -1)/(r -1)
 total :: Integer
-total = sum (map square [1..64])
+total = 1 * (2 ^ (64 :: Int) - 1) `div` (2 -1)
