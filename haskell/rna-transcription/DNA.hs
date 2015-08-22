b9bb5c11924ca565b878b6c2c2b485d70061facf
rojoangel <rojo.angel@gmail.com>
2015-08-22 21:24:57 +0200

    haskell - rna-transcription - it. 5 using lookup

--- haskell/rna-transcription/DNA.hs
+++ haskell/rna-transcription/DNA.hs
@@ -1,9 +1,10 @@
 module DNA (toRNA) where
 
-toRNA :: String -> String
-toRNA xs = map transcribe xs
-  where transcribe x = case x of 'C' -> 'G'
-                                 'G' -> 'C'
-                                 'A' -> 'U'
-                                 'T' -> 'A'
-                                 _   -> error "invalid nucleotide"
+import Data.Maybe
+
+toRNA :: [Char] -> [Char]
+toRNA = mapMaybe $ flip lookup transcriptions
+    where transcriptions = [('C', 'G'),
+                            ('G', 'C'),
+                            ('A', 'U'),
+                            ('T', 'A')]
