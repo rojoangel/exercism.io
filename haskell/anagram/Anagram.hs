8d38734d2b6f0cc72e4a406a247dd91f572b33a9
rojoangel <rojo.angel@gmail.com>
2015-08-23 23:27:05 +0200

    haskell - anagram - it. 2 extracted some logic to where

--- haskell/anagram/Anagram.hs
+++ haskell/anagram/Anagram.hs
@@ -4,4 +4,6 @@ import Data.List (sort)
 import Data.Char (toLower)
 
 anagramsFor :: String -> [String] -> [String]
-anagramsFor s xs = [x | x <- xs, map toLower s /= map toLower x && sort (map toLower s) == sort (map toLower x)]
+anagramsFor s xs = [x | x <- xs, not $ itsOwnAnagram s x, isAnagram s x]
+  where isAnagram x y = sort (map toLower x) == sort (map toLower y)
+        itsOwnAnagram x y = map toLower x == map toLower y
