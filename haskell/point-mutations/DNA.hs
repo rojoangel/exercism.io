4fe61c5f0198c4f4493783bc4abd4695bf3c8fb8
rojoangel <rojo.angel@gmail.com>
2015-08-25 01:37:48 +0200

    haskell - point-mutations - it. 2 avoid using list comprehension

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,5 +1,5 @@
 module DNA where
 
-hammingDistance :: [Char] -> [Char] -> Int
-hammingDistance xs ys = sum [ 1 | (x,y) <- zip xs ys, x /= y ]
-
+hammingDistance :: String -> String -> Int
+hammingDistance xs ys = length $ filter (uncurry (/=)) $ zip xs ys
+-- hammingDistance xs ys = sum [ 1 | (x,y) <- zip xs ys, x /= y ]
