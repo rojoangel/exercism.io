54f420924680b39461cdc0bee7fd794422493d64
rojoangel <rojo.angel@gmail.com>
2015-08-26 21:05:50 +0200

    haskell - point-mutations - it. 4 use map fromEnum

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,5 +1,6 @@
 module DNA where
 
 hammingDistance :: String -> String -> Int
-hammingDistance xs ys = length $ filter id $ zipWith (/=) xs ys
+hammingDistance xs ys = sum $ map fromEnum $ zipWith (/=) xs ys
+-- hammingDistance xs ys = length $ filter id $ zipWith (/=) xs ys
 -- hammingDistance xs ys = sum [ 1 | (x,y) <- zip xs ys, x /= y ]
