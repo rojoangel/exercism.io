c3dbc19814828f2f5b44c7a9ac64ea535c80563d
rojoangel <rojo.angel@gmail.com>
2015-08-26 20:41:33 +0200

    haskell - point-mutations - it. 3 get rid of uncurry by using zipWith

--- haskell/point-mutations/DNA.hs
+++ haskell/point-mutations/DNA.hs
@@ -1,5 +1,5 @@
 module DNA where
 
 hammingDistance :: String -> String -> Int
-hammingDistance xs ys = length $ filter (uncurry (/=)) $ zip xs ys
+hammingDistance xs ys = length $ filter id $ zipWith (/=) xs ys
 -- hammingDistance xs ys = sum [ 1 | (x,y) <- zip xs ys, x /= y ]
